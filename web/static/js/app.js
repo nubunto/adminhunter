0a50a46ee7c335496aef63a791347995b39c8229
Bruno Panuto <bl.panuto@gmail.com>
2016-04-21 04:00:28 -0300

    refurbished interface for client viewer/editor

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,3 +12,34 @@ fl.click(function() {
     $(this).parent().find("div").toggle();
   }
 });
+window.addEventListener('load', function() {
+	$('.remove-grp').click(function() {
+		$(this).parent().remove();
+	});
+	$("#phone-add").click(function() {
+		var grp = $('<div/>').attr('class', 'input-group');
+		var input = $("<input/>")
+			.attr('type', 'phone')
+			.attr('name', 'client[contact][phones][]')
+			.attr('class', 'form-control')
+		var rem_btn = $('<a/>').attr('role', 'button').attr('class', 'input-group-addon').text('-');
+		rem_btn.on('click', function(){
+			grp.remove();
+		});
+		grp.append(input).append(rem_btn);
+		grp.insertBefore($(this));
+	});
+	$("#email-add").click(function() {
+		var grp = $('<div/>').attr('class', 'input-group');
+		var input = $("<input/>")
+			.attr('type', 'email')
+			.attr('name', 'client[contact][emails][]')
+			.attr('class', 'form-control')
+		var rem_btn = $('<a/>').attr('role', 'button').attr('class', 'input-group-addon').text('-');
+		rem_btn.on('click', function(){
+			grp.remove();
+		});
+		grp.append(input).append(rem_btn);
+		grp.insertBefore($(this));
+	});
+});
